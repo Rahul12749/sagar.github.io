004b8b8de9af33b047ee1f03c1b4a7e07bf2be71
Rahul12749 <74234883+Rahul12749@users.noreply.github.com>
2023-10-26 12:12:56 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+    "use strict";
+        // PARALLAX JS
+        function initParallax() {
+          $('#home').parallax("60%", 100);
+          $('#about').parallax("100%", 80);
+          $('#project').parallax("80%", 60);
+          $('#team').parallax("40%", 40);
+          $('#contact').parallax("20%", 20);
+          }
+        initParallax();
+})(jQuery);
