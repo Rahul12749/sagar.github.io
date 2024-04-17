ab3dcd6163440f5a2321fab93a9a9234121f184e
unknown <jainr8488@gmail.com>
2024-04-17 09:41:16 +0530

    asd

--- js/main.js
+++ js/main.js
@@ -2,33 +2,37 @@
 
 	'use strict';
 
-  // $('.site-menu-toggle').click(function(){
-  //   var $this = $(this);
-  //   if ( $('body').hasClass('menu-open') ) {
-  //     $this.removeClass('open');
-  //     $('.js-site-navbar').fadeOut(400);
-  //     $('body').removeClass('menu-open');
-  //   } else {
-  //     $this.addClass('open');
-  //     $('.js-site-navbar').fadeIn(400);
-  //     $('body').addClass('menu-open');
-  //   }
-  // });
+  $('.site-menu-toggle').click(function(){
+    var $this = $(this);
+    if ( $('body').hasClass('menu-open') ) {
+      $this.removeClass('open');
+      $('.js-site-navbar').fadeOut(400);
+      $('body').removeClass('menu-open');
+    } else {
+      $this.addClass('open');
+      $('.js-site-navbar').fadeIn(400);
+      $('body').addClass('menu-open');
+    }
+  });
 
 	
-	// $('nav .dropdown').hover(function(){
-	// 	var $this = $(this);
-	// 	$this.addClass('show');
-	// 	$this.find('> a').attr('aria-expanded', true);
-	// 	$this.find('.dropdown-menu').addClass('show');
-	// }, function(){
-	// 	var $this = $(this);
-	// 		$this.removeClass('show');
-	// 		$this.find('> a').attr('aria-expanded', false);
-	// 		$this.find('.dropdown-menu').removeClass('show');
-	// });
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
 
-  
+
+
+	$('#dropdown04').on('show.bs.dropdown', function () {
+	  console.log('show');
+	});
   /**
    * Easy selector helper function
    */
